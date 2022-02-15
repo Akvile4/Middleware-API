02141df1869b515c8bb1080da65e316d8d115d2e
Akvile4 <aqua0818@gmail.com>
2022-02-15 21:29:04 +0200

    added middleware function

---
+++ src/user/userControllers.js
@@ -0,0 +1,14 @@
+const User = require("./userModel");
+
+exports.addUser = async (req, res) => {
+    try {
+        const newUser = await User.create(req.body);
+            // like return statement
+        res.status(200).send({ user: newUser });
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({err: error.message});
+    }
+};
+
+// mix express and mongoose
