7747b7ec5f89f897ac6d9f382c7e33aec87ebcea
Akvile4 <aqua0818@gmail.com>
2022-02-16 13:49:25 +0200

    added http methods to user

--- src/user/userControllers.js
+++ src/user/userControllers.js
@@ -6,9 +6,44 @@ exports.addUser = async (req, res) => {
             // like return statement
         res.status(200).send({ user: newUser });
     } catch (error) {
-        console.log(error)
+        console.log(error);
         res.status(500).send({err: error.message});
     }
 };
+
+    // will show all users in a database
+exports.listUsers = async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.status(200).send({ users });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({err: error.message});
+    }
+};
+
+    // updates username
+exports.updateUser = async (req, res) => {
+    try {
+        const user = await User.updateOne({ username: req.body.username },
+            { $set: { username: req.body.newUsername } });
+            res.status(200).send({ user });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({err: error.message});
+    }
+};
+
+    // you can delete by name or email
+exports.deleteUser = async (req, res) => {
+    try {
+        const user = await User.deleteOne(req.body);
+        res.status(200).send({ user });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({err: error.message});
+    }
+};
+
 
 // mix express and mongoose
--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,9 +1,15 @@
 const { Router } =require("express");
-const { addUser } =require("./userControllers");
+const { addUser, listUsers, updateUser, deleteUser } =require("./userControllers");
 const { hashPass } = require("../middleware");
 const userRouter = Router();
 
+
+    // hashPass is middleware (literally because in the middle)
+    // we can add as much middleware as we want 
 userRouter.post("/user", hashPass, addUser);
+userRouter.get("/user", listUsers);
+userRouter.put("/user", updateUser);
+userRouter.delete("/user", deleteUser);
 
 // userRouter.post("/user", addUser);
 
